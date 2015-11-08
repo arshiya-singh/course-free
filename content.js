76dec8bd9c506fcfac43442414893759244bdea3
as1216 <arshiya.singh16@gmail.com>
2015-11-08 02:41:03 -0500

    update everything

--- content.js
+++ content.js
@@ -6,8 +6,34 @@ $(document).ready( function(){
             console.log(className);
             $(this).attr('data-trigger','hover');
             $(this).attr('data-container','body');
-            searchableName = className.replace(/ /g,'').toLowerCase();//name to use in search
-            $(this).popover({title: '<h1><strong>'+className+'</strong></h1>',
-                content: '<h2>'+ +'</h2>', html: true, placement: 'right'});
+            searchableName = className.replace(/ |&/g,'').toLowerCase();//name to use in search
+            var url = "https://api.coursera.org/api/catalog.v1/courses?q=search&query=" + searchableName;
+            var $this = $(this);
+            console.log(url);
+            getSites(url, className, $this);
     })
+
+    function getSites(url, className,variable) {
+        $.ajax({
+            'url' : url,
+            'type' : 'GET',
+            'success' : function(data) {
+              if (data != null) {
+                var entry = "";
+                data.elements.forEach(function(element) {
+                    entry = entry + element.name + "<br/>";
+                });
+                variable.popover({title: '<h1><strong>'+className+'</strong></h1>', 
+                    content: '<h4>'+ entry +'</h4>', html: true, placement: 'right'});
+                }
+            },
+
+            'error' : function() {
+                var copyName= className;
+                copyName = copyName.trim().split(" ");
+                url = "https://api.coursera.org/api/catalog.v1/courses?q=search&query=" +copyName[0];
+                getSites(url, className, variable);
+            }
+        });
+    }
 });
