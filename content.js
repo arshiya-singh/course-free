385db3dbb7a8aa3e9a936f7c006b3a4cf3751a86
as1216 <arshiya.singh16@gmail.com>
2015-11-07 23:52:55 -0500

    add new library files and update files

--- content.js
+++ content.js
@@ -1,24 +1,13 @@
-$(document).ready(function() {
-    $('course-list name').hover {
-
-        function () {
-            var className = document.getElementsByClassName("name").lastChild();
-            var tokenizer = className.split(-);
+$(document).ready( function(){
+    $('.course-list .name').hover(function() {
+            var className = $(this).text();
+            var tokenizer = className.split('-');
             className = tokenizer[1]; 
-
-            $('course-list name:nth-child(' + ($(this).index() + 1) + ')').addClass('hover');
-            $(".hover").append();
-        }
-    }
-    function () {
-        $('course-list name:nth-child(' + ($(this).index() + 1) + ')').removeClass('hover');
-    }
-
-     $('.something').mouseover(function() {
-        $('.popup p').show();
-    });
-
-    $('.something').mouseout(function() {
-        $('.popup p').hide();
-    });
+            console.log(className);
+            $(this).attr('data-trigger','hover');
+            $(this).attr('data-container','body');
+            searchableName = className.replace(/ /g,'').toLowerCase();//name to use in search
+            $(this).popover({title: '<h1><strong>'+className+'</strong></h1>',
+                content: '<h2>'+ +'</h2>', html: true, placement: 'right'});
+    })
 });
