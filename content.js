3070c3456c2a6a4787320cfdb67bb1d9bc533fb4
as1216 <arshiya.singh16@gmail.com>
2015-11-07 20:53:14 -0500

    add all changes

--- content.js
+++ content.js
@@ -1,12 +1,24 @@
-$(document).ready() {
-    $('table course-box').hover {
-        function () {
-            $('table course-box:nth-child(' + ($(this).index() + 1) + ')').addClass('hover');
-        },
+$(document).ready(function() {
+    $('course-list name').hover {
 
         function () {
-            $('table course-box:nth-child(' + ($(this).index() + 1) + ')').removeClass('hover');
+            var className = document.getElementsByClassName("name").lastChild();
+            var tokenizer = className.split(-);
+            className = tokenizer[1]; 
+
+            $('course-list name:nth-child(' + ($(this).index() + 1) + ')').addClass('hover');
+            $(".hover").append();
         }
     }
-    //add hover box
-}
+    function () {
+        $('course-list name:nth-child(' + ($(this).index() + 1) + ')').removeClass('hover');
+    }
+
+     $('.something').mouseover(function() {
+        $('.popup p').show();
+    });
+
+    $('.something').mouseout(function() {
+        $('.popup p').hide();
+    });
+});
