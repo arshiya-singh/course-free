5af5c3ddc1ff965ee6098ae73cd6ff19ae8d036d
Karthik Praturu <kart.prat@gmail.com>
2015-11-08 05:42:15 -0500

    UI Changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CourseFree
-A chrome extension which lists alternative MOOCs from Coursera when one hovers over their courses in courseoff.com
+# hackduke15
+A chrome extension which lists alternative MOOCs for the corresponding courses in courseoff.com
--- content.js
+++ content.js
@@ -4,14 +4,14 @@ $(document).ready( function(){
             var tokenizer = className.split('-');
             className = tokenizer[1]; 
             console.log(className);
-            $(this).attr('data-trigger','hover');
+            $(this).attr('data-trigger','click');
             $(this).attr('data-container','body');
             searchableName = className.replace(/ |&/g,'').toLowerCase();//name to use in search
-            var url = "https://api.coursera.org/api/catalog.v1/courses?q=search&query=" + searchableName;
+            var url = "https://api.coursera.org/api/catalog.v1/courses?fields=video&q=search&query=" + searchableName;
             var $this = $(this);
             console.log(url);
             getSites(url, className, $this);
-    })
+    });
 
     function getSites(url, className,variable) {
         $.ajax({
@@ -21,17 +21,17 @@ $(document).ready( function(){
               if (data != null) {
                 var entry = "";
                 data.elements.forEach(function(element) {
-                    entry = entry + element.name + "<br/>";
+                    entry = entry + '<a target="_tab" href='+"https://youtube.com/watch?v="+element.video+'>'+ element.name + '</a>' + "<br/>";
                 });
                 variable.popover({title: '<h1><strong>'+className+'</strong></h1>', 
-                    content: '<h4>'+ entry +'</h4>', html: true, placement: 'right'});
+                    content: '<p>'+ entry + '<br/><p>', html: true, placement: 'right'});
                 }
             },
 
             'error' : function() {
                 var copyName= className;
                 copyName = copyName.trim().split(" ");
-                url = "https://api.coursera.org/api/catalog.v1/courses?q=search&query=" +copyName[0];
+                url = "https://api.coursera.org/api/catalog.v1/courses?fields=video&q=search&query=" +copyName[0];
                 getSites(url, className, variable);
             }
         });
